af1a9ff00ce9a69914c059a1e662feb893795ba6
rgarcia-herrera <rgarcia@inmegen.gob.mx>
2016-03-23 17:50:42 -0500

    CPU contabiliza quantum

--- fake_scheduler/__init__.py
+++ fake_scheduler/__init__.py
@@ -23,31 +23,56 @@ class Entorno(object):
         self.t+= 1
 
 
+
     def __repr__(self):
         return "%s %s" % (self.t, [p for p in self.procesadores])
         
 class Procesador(object):
-    def __init__(self, tiempo_cambio=10):
+    def __init__(self, tiempo_cambio=10, quantum=100):
         self.tiempo_cambio  = tiempo_cambio
-        self.proceso = None
-        self.status  = 'idle'
-        self.tct     = tiempo_cambio
+        self.quantum        = quantum
+        self.proceso        = None
+        self.status         = 'idle'
+        self.tct            = tiempo_cambio
+        self.q              = 0
 
     def procesa(self):
+        # en tiempos de cambio de contexto no se procesa nada
         if self.status == 'TCT':
             self.tct -= 1
             if self.tct == 0:
                 self.status = 'idle'
                 self.tct = self.tiempo_cambio
+                
         elif self.proceso and self.status == 'R':
+            
             self.proceso.ejecuta()
+            
+            # contabiliza el quantum
+            self.q += 1
+            
             # tras ejecutarlo una unidad vemos si no ha terminado
             if self.proceso.status == 'F':
                 # quitamos proceso terminado
                 self.proceso = None
                 # al terminar un proceso hay que entrar en tiempo de cambio de contexto
                 self.status  = 'TCT'
+            elif self.q >= self.quantum: # habra agotado su quantum?
+                # resetea contador de quantum
+                self.q = 0
+                # proceso en status Wait
+                self.proceso.status = 'W'
 
+                # si el proceso es echado por agotamiento de quantum
+                # mientras esta bloqueado, hay que regresarle el
+                # bloqueo interrumpido
+                if self.proceso.status == 'B':
+                    self.proceso.bloqueos += 1
+                self.proceso = None
+                self.status  = 'TCT'
+
+            
+                
     def __repr__(self):
         if self.proceso:
             return "<cpu %s %s>" % (self.status, self.proceso)
@@ -64,7 +89,7 @@ class Proceso(object):
         self.tiempo_bloqueo = tiempo_bloqueo        
         self.tiempo_de_ejecucion = 0
         self.tiempo_bloqueado    = 0
-        self.status   = None
+        self.status   = 'W'
 
     def __repr__(self):
         return "(pid=%s st=%s d=%s t=%s d-t=%s)" % (self.pid, self.status, self.duracion,
@@ -72,6 +97,7 @@ class Proceso(object):
 
 
     def ejecuta(self):
+        
         if self.status == 'R':
             # mientras estE corriendo y queden bloqueos pendientes,
             # tal vez hay que bloquear
@@ -87,8 +113,11 @@ class Proceso(object):
                 if self.tiempo_de_ejecucion == self.duracion:
                     self.status = 'F'
 
+
+        # si el proceso esta bloqueado aumentar el contador de bloqueo
         if self.status == 'B':
             self.tiempo_bloqueado += 1
 
-            if self.tiempo_bloqueado == self.tiempo_bloqueo:
+            # si alcanzo el tiempo de bloqueo, desbloquear
+            if self.tiempo_bloqueado > self.tiempo_bloqueo:
                 self.status = 'R'
--- sim_tarea11.py
+++ sim_tarea11.py
@@ -7,15 +7,15 @@ from time import sleep
 e2 = Entorno()
 
 # el entorno 2 tiene dos procesadores
-e2.procesadores = [ Procesador(tiempo_cambio=3),
-                    Procesador(tiempo_cambio=3) ] # , Procesador(), Procesador() ]
+e2.procesadores = [ Procesador(tiempo_cambio=2, quantum=4), ]
+#                    Procesador(tiempo_cambio=3, quantum=3) ]
 
 
 
 queue = [
-    Proceso("A", duracion=7,  inicio=0, bloqueos=2, tiempo_bloqueo=5),
-    Proceso("B", duracion=6,  inicio=0, bloqueos=2, tiempo_bloqueo=5),
-    Proceso("C", duracion=4,  inicio=0, bloqueos=2, tiempo_bloqueo=5),
+    Proceso("A", duracion=7,  inicio=3, bloqueos=1, tiempo_bloqueo=2),
+#    Proceso("B", duracion=6,  inicio=0, bloqueos=2, tiempo_bloqueo=5),
+#    Proceso("C", duracion=4,  inicio=0, bloqueos=2, tiempo_bloqueo=5),
     # Proceso("D", duracion=100,  inicio=0,    bloqueos=2),
     # Proceso("E", duracion=1000, inicio=3000, bloqueos=5),
     # Proceso("F", duracion=500,  inicio=0,    bloqueos=3),
